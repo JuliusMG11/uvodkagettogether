7211f85d1e90777f7109db59f3d4afbebc6fa2d9
JuliusMG11 <33003180+JuliusMG11@users.noreply.github.com>
2021-09-04 14:07:06 +0200

    mobile menu

---
+++ src/components/UI/linkExitIcon.vue
@@ -0,0 +1,16 @@
+<template>
+      <svg xmlns="http://www.w3.org/2000/svg" width="512" height="425.488" viewBox="0 0 512 425.488">
+  <g id="undo_1_" data-name="undo(1)" transform="translate(0 -43.256)">
+    <g id="Group_19250" data-name="Group 19250">
+      <g id="Group_19249" data-name="Group 19249">
+        <path id="Path_17210" data-name="Path 17210" d="M142.716,293.147l-94-107.6,94-107.6a20.926,20.926,0,1,0-31.522-27.532L5.169,171.782a20.915,20.915,0,0,0,0,27.532L111.195,320.686a20.928,20.928,0,0,0,31.521-27.539Z" fill="#fff"/>
+      </g>
+    </g>
+    <g id="Group_19252" data-name="Group 19252">
+      <g id="Group_19251" data-name="Group 19251">
+        <path id="Path_17211" data-name="Path 17211" d="M359.93,164.619h-339a20.926,20.926,0,1,0,0,41.852h339a110.211,110.211,0,1,1,0,220.422H48.828a20.926,20.926,0,1,0,0,41.852h311.1c83.844,0,152.07-68.219,152.07-152.063S443.781,164.619,359.93,164.619Z" fill="#fff"/>
+      </g>
+    </g>
+  </g>
+</svg>
+</template>
--- src/components/nav/navigation.vue
+++ src/components/nav/navigation.vue
@@ -1,21 +1,33 @@
 <template>
   <div id="nav">
     <div class="navigation">
-      <div class="section-logo">
-        <img src="@/assets/svg/logo.svg" />
-      </div>
-
-      <div class="section-navigation">
-        <router-link to="/">Úvod</router-link>
-        <router-link to="/our-service">Naša služby</router-link>
-        <router-link to="/about">O nás</router-link>
-        <router-link to="/contact">Kontakt</router-link>
+      <div class="container">
+        <div class="column-30">
+          <div class="section-logo">
+            <img src="@/assets/svg/logo.svg" />
+          </div>
+        </div>
+        <div class="column-70">
+          <div class="section-navigation">
+            <router-link to="/">Úvod</router-link>
+            <router-link to="/our-service">Naša služby</router-link>
+            <router-link to="/about">O nás</router-link>
+            <router-link to="/contact">Kontakt</router-link>
+          </div>
+        </div>
       </div>
     </div>
 
-    <div class="mobile-section-navigation">
+    <div class="mobile-section-navigation" :class="{ active: isActive }">
+
+      <div class="exit-button">
+        <link-exit-icon 
+          @click="exitMenu()"
+        />
+      </div>
+
       <div class="navigation-content">
-      <router-link to="/">Úvod</router-link>
+          <router-link to="/">Úvod</router-link>
           <router-link to="/our-service">Naša služby</router-link>
           <router-link to="/about">O nás</router-link>
           <router-link to="/contact">Kontakt</router-link>
@@ -26,15 +38,35 @@
 </template>
 
 <script>
+import linkExitIcon from '../UI/linkExitIcon.vue';
+
 export default {
-  components: {},
+  
+  data() {
+    return {
+       isActive: true
+    }
+  },
+
+  components: {
+
+    linkExitIcon
+
+  },
+  methods: {
+    exitMenu() {
+
+
+      this.isActive = !this.isActive;
+      
+      console.log('debil');
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 #nav {
-  max-width: 1400px;
-  margin: 0 auto;
 
   a {
     font-weight: bold;
@@ -52,15 +84,12 @@ export default {
   }
 
   .navigation {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-
-     position: fixed;
-      width: 1420px;
+      position: fixed;
+      width: 100%;
       z-index: 20;
-      background: rgba(255, 255, 255, 0.801);
-      padding: 10px;
+      background: rgba(255, 255, 255, 0.932);
+      padding-top: 10px;
+      padding-bottom: 10px;
       border-radius: 0px 0px 20px 20px;
 
     a {
@@ -68,6 +97,10 @@ export default {
     }
 
     .section-navigation {
+      display: flex;
+      justify-content: end;
+      align-items: center;
+      height: 100%;
     
       &::after {
         content: '';
@@ -83,6 +116,11 @@ export default {
     }
 
     .section-logo {
+
+      display: flex;
+      justify-content: start;
+      align-items: center;
+
       img {
         max-width: 250px;
       }
@@ -90,21 +128,56 @@ export default {
   }
 
   .mobile-section-navigation {
+
     display: flex;
     position: fixed;
+    align-items: center;
+    justify-content: center;
     left: 0px;
     bottom: 0px;
-    flex-direction: center;
     padding: 40px 30px 40px 30px;
-    background: #12121260;
+    background: #121212c5;
     width: 100%;
     height: 100%;
-    display: none;
+    z-index: 20;
+
+    .exit-button {
+      position: absolute;
+      top: 40px;
+      right: 40px;
+
+      svg {
+        width: 60px;
+        height: 60px;
+      }
+
+    }
 
     .navigation-content {
-      justify-content: center;
       display: flex;
+        justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      
+      a {
+        margin-right: 0px;
+        font-size: 52px;
+        padding-top: 20px;
+        color: white;
+
+        &:first-child {
+          padding-top: 0px;
+        }
+      }
     }
+  }
+
+  .isActive {
+    display: none;
+  }
+
+  .nonactve {
+    display: none;
   }
 }
 </style>
